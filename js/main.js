49389f11d24f945e77bfd02aed218f94bc0a48fa
Amir <72108341+Amirali-Khamseh@users.noreply.github.com>
2023-07-20 13:44:20 +0200

    Mobile nav menu done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-/*=============== SHOW MENU ===============*/
+const navMenu = document.querySelector('#nav-menu');
+const navToggle = document.querySelector('#nav-toggle');
+const navClose = document.querySelector('#nav-close');
+const navLinks = document.querySelectorAll('.nav-link');
+
+//Removing the mobile menu upon clicking any link
+const linkRemoval = ()=>{
+    navMenu.classList.remove('show-menu');
+}
+navLinks.forEach(n=>n.addEventListener('click',linkRemoval));
 
 
-/*=============== REMOVE MENU MOBILE ===============*/
+//Showing and hiding the menu 
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu');
+    })
+}
 
+if(navClose){
+    navClose.addEventListener('click',()=>{
+       navMenu.classList.remove('show-menu');
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-
-
-/*=============== GSAP ANIMATION ===============*/
+    })
+}
